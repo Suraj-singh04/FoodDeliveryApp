c245d16a1186ebaa8ef7acc1d7ac4fa0e2c1f7e5
Suraj-singh04 <surajsingh877028@gmail.com>
2025-11-05 02:13:18 +0530

    Created Sign-in and Sign-up page and some custom resuable components for the app

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.fastfood.ordering",
+  databaseId: "690a3c00003b4a0957c3",
+  userCollectionId: "user",
+};
